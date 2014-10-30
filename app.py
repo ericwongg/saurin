8b57d287e3aa8ed9d8024e5d2e3edb783fcac395
Eric Wong <wonggeric@gmail.com>
2014-10-30 11:48:04 -0400

    started

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+login = False
+
 @app.route("/")
 def main():
-    post_button = request.args.get("post_button",None)
-    
-    if post_button == login:
-        render_template("login.html")
-    elif post_button == register:
-        render_template("register.html")
-    else:
-        render_template("main.html")
+    render_template("main.html")
+
+@app.route("/login", method=["GET","POST"])
+
+@app.route("/user")
+#private pages
+def user():
+    if (login == False):
+        return
+        
+
+@app.route("/public")
+#public pages
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

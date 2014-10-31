2165d3e4d3acaf3daec4e8b7ed3691045674b1a5
Eric Wong <wonggeric@gmail.com>
2014-10-31 11:43:51 -0400

    made insertUser function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import mongo
 
 app = Flask(__name__)
 
@@ -9,6 +10,13 @@ def main():
     render_template("main.html")
 
 @app.route("/login", method=["GET","POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        user_id = request.args.get("uname",None)
+        password = request.args.get("pass",None)
+        button = request.args.get("b",None)
+        if button
 
 @app.route("/user")
 #private pages

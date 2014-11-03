33c5e73e735f94f5165dd200684b3535051b3cf7
Jerry Dai <jdai315@gmail.com>
2014-11-03 11:39:18 -0500

    python skeleton done

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Eric Wong
 Period 5
 
 account manager project
+this is gr9
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-import mongo
+import pymongo
 
 app = Flask(__name__)
 
@@ -7,34 +7,80 @@ login = False
 
 @app.route("/", method=["GET", "POST"])
 def main():
-    return render_template("main.html")
     button = request.args.get("b",None)
-    if button == login:
+    if button == 'login':
         login()
-    elif button == regist:
+    elif button == 'regist':
         register()
     else:
         return render_template("main.html")
 
 @app.route("/login", method=["GET","POST"])
 def login():
-    error = None
+    user_id = request.args.get("uname",None)
+    password = request.args.get("pass",None)
     if request.method == 'POST':
-        user_id = request.args.get("uname",None)
-        password = request.args.get("pass",None)
-        
+        login = True
+        #check if matches database
+        user()
+    else:
+        flash("Invalid Username or Password!")
+        return render_template("login.html")
 
-@app.route("/user")
+@app.route("/logout", method=["GET","POST"])
+def logout():
+    button = request.args.get("b",None)
+    login = False
+    if button == 'home':
+        main()
+    else:
+        return render_template("logout.html")
+    
+@app.route("/register", method=["GET","POST"])
+def register():
+    user_id = request.args.get("uname",None)
+    password = request.args.get("pass",None)
+    if request.method == "POST":
+        #put new data into database
+        flash("Successfully registered!")
+        return render_template("login.html")
+    else:
+        return redirect("/register")
+
+@app.route("/user1", method=["GET","POST"]))
 #private pages
-def user():
+def user1():
     if (login == False):
-        return
-        
+        button = request.args.get("b",None)
+        if button == 'next':
+            user2()
+        else:
+            return render_template("user1.html")
+    else:
+        return redirect("/main")
+    
+@app.route("/user2", method=["GET","POST"]))
+#private pages
+def user2():
+    if (login == False):
+        button = request.args.get("b",None)
+        if button == 'next':
+            user1()
+        else:
+            return render_template("user2.html")
+    else:
+        return redirect("/main")
 
-@app.route("/public")
+@app.route("/public", method=["GET","POST"])
 #public pages
+def public():
+    button = request.args.get("b", None)
+    if button == 'home':
+        main()
+    else:
+        return render_template("public.html")
 
-
+#main
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host="0.0.0.0", port=1234)

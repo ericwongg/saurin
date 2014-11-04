a57eb0f9e974675c55cee2bf3abb34ce4703848e
unknown <jdai315@gmail.com>
2014-11-04 17:36:06 -0500

    ****

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, flash, session
-import data
+import pymongo, data
 
 app = Flask(__name__)
-app.session_interface = MongoSessionInterface(db='saurin')
+#app.session_interface = MongoSessionInterface(db='saurin')
 
 login = False
 
-@app.route("/", method=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def main():
     button = request.args.get("b",None)
     if button == 'login':
@@ -16,77 +16,72 @@ def main():
     else:
         return render_template("main.html")
 
-@app.route("/login", method=["GET","POST"])
+@app.route("/login", methods=["GET","POST"])
 def login():
-    user_id = request.args.get("uname",None)
-    password = request.args.get("pass",None)
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        user_id = request.args.get("uname",None)
+        password = request.args.get("pass",None)
         login = True
         #check login, send to private page if successful
         if data.check(user_id, password):
             private1()
         else:
             flash("Invalid Username or Password!")
-            return render_template("login.html")
-
-@app.route("/logout", method=["GET","POST"])
+            return redirect("/login")
+    
+@app.route("/logout", methods=["GET","POST"])
 def logout():
     button = request.args.get("b",None)
     login = False
     if button == 'home':
         flash("Successfully logged out")
-        main()
+        return main()
     else:
         return render_template("logout.html")
     
-@app.route("/register", method=["GET","POST"])
+@app.route("/register", methods=["GET","POST"])
 def register():
-    user_id = request.args.get("uname",None)
-    password = request.args.get("pass",None)
-    if request.method == "POST":
-        #need to check if username in database already
-        #true if user not in, false otherwise
-        #if separate.checkuser(user_id):
-        #flash("Successfully registered!")
-        #return render_template("login.html")
-        #else:
-        #flash("Sorry, the username is already taken")
-        #return redirect("/register")
-        #put new data into database
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        user_id = request.args.get("uname",None)
+        password = request.args.get("pass",None)
+        #add new data to db, if taken dont do anything
         if data.addNew(user_id, password):
             flash("Successfully registered!")
             return render_template("login.html")
         else:
             flash("Sorry, the username is already taken.")
             return redirect("/register")
-    else:
-        return redirect("/register")
 
-@app.route("/user1", method=["GET","POST"]))
+
+@app.route("/private1", methods=["GET","POST"])
 #private pages
 def private1():
     if (login == False):
         button = request.args.get("b",None)
         if button == 'next':
-            user2()
+            private2()
         else:
             return render_template("private1.html")
     else:
         return redirect("/main")
     
-@app.route("/user2", method=["GET","POST"]))
+@app.route("/private2", methods=["GET","POST"])
 #private pages
 def private2():
     if (login == False):
         button = request.args.get("b",None)
         if button == 'next':
-            user1()
+            private1()
         else:
             return render_template("private2.html")
     else:
         return redirect("/main")
 
-@app.route("/public", method=["GET","POST"])
+@app.route("/public", methods=["GET","POST"])
 #public pages
 def public():
     button = request.args.get("b", None)
